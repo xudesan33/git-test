e8e881a118b027271b3b05a4252453497fc637be
xudesan33 <2913761481@qq.com>
2020-11-27 16:51:20 +0800

    fix bug

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("second");
         System.out.println("third");
+        System.out.println("hot fix");
     }
 }
